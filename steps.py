5de87acb42a772fc8f052a30975d036a6b91e2db
Sasha Segal <wester.55@gmail.com>
2015-10-06 16:00:04 +0300

    commit

--- steps.py
+++ steps.py
@@ -7,7 +7,7 @@ import os
 vagrantfile_url = "https://raw.githubusercontent.com/wester55/tw_reader/master/Vagrantfile"
 filter_command = "-s -X GET 'http://localhost:27080/twitter/messages/_find' | grep -Po '\"screen_name\":.*?[^\\]\",' | awk '{print $2}' | cut -d'\"\' -f2"
 
-if len(sys.argv) != 1:
+if len(sys.argv) != 2:
     print "Only one argument allowed, either \"setup\" or \"run\" string must be specified"
     exit(1)
 
