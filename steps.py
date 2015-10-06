78988251fa4f6dff99b26fa268515487588f1f8b
Sasha Segal <wester.55@gmail.com>
2015-10-06 17:01:26 +0300

    commit

--- steps.py
+++ steps.py
@@ -5,6 +5,7 @@ import subprocess
 import os
 
 vagrantfile_url = "https://raw.githubusercontent.com/wester55/tw_reader/master/Vagrantfile"
+setup_command1 = "/usr/bin/curl " + vagrantfile_url + " -o Vagrantfile"
 run_command1 = "curl --data server=localhost:27017 'http://localhost:27080/_connect'"
 run_command2 = "curl -s -X GET 'http://localhost:27080/twitter/messages/_find' | grep -Po '\"screen_name\":.*?[^\\\]\",' | awk '{print $2}' | cut -d'\"' -f2"
 
@@ -25,13 +26,14 @@ if sys.argv[1] == "setup":
         pass
     os.chdir(fldr)
     subprocess.call([vagrant, "init"])
-    subprocess.call(["/usr/bin/curl", vagrantfile_url + " -o Vagrantfile"])
+    p = subprocess.Popen(setup_command1, shell=True)
+    output, err = p.communicate()
     subprocess.call([vagrant, "up"])
 elif sys.argv[1] == "run":
-    p = subprocess.Popen(run_command1, shell=True, stderr=subprocess.PIPE)
+    p = subprocess.Popen(run_command1, shell=True)
     output, err = p.communicate()
     print ""
-    p = subprocess.Popen(run_command2, shell=True, stderr=subprocess.PIPE)
+    p = subprocess.Popen(run_command2, shell=True)
     output, err = p.communicate()
 else:
     print "Only one argument allowed, either \"setup\" or \"run\" string must be specified"
